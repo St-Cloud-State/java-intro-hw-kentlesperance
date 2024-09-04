a2af0240c167b1f981d37c900c3a784ae5161307
kentlesperance <126631169+kentlesperance@users.noreply.github.com>
2024-09-04 23:44:29 +0000

    6:45 Sept 4 2024. Hw1Q2

---
+++ MyMain.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class MyMain {
+    public static void main(String[] args) {
+        LinkedList<Person> personList = new LinkedList<>();
+        
+        try { 
+            FileInputStream fileInput = new FileInputStream("data.txt");
+            store(fileInput, personList); //store method called
+            fileInput.close();
+        } catch(IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            return;
+        }
+        
+
+
+        System.out.println("All Persons:");
+        display(System.out, personList); //display method called
+
+        System.out.println(find("1793", personList));
+        System.out.println(find("0", personList));
+        System.out.println(find("999", personList));
+        System.out.println(find("1542", personList));
+    }
+
+    //Reads the data for several persons from the input stream and stores the data in the linked list.
+    public static void store(InputStream input, LinkedList<Person> list) {
+        Scanner scanner = new Scanner(input);
+        while (scanner.hasNextLine()) {
+            String[] data = scanner.nextLine().split(", ");
+            if (data.length == 3) {
+                list.add(new Person(data[0], data[1], data[2]));
+            }
+        }
+        scanner.close();
+    }
+
+    //Writes the data for all person objects in the linked list, one per line
+    public static void display(PrintStream output, LinkedList<Person> list) {
+        for (Person person : list) {
+            output.println(person);
+        }
+    }
+
+    //Returns the index of the person object in the linked list, that has the same id value as sid (returns -1 is no such person exists).
+    public static int find(String sid, LinkedList<Person> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId().equals(sid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Person.java
@@ -0,0 +1,32 @@
+public class Person {
+    //String fields
+    private String firstName;
+    private String lastName;
+    private String id; //id is kept unique by the data class
+
+    public Person(String firstName, String lastName, String id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    //Accessor methods
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName + " , ID: " + id;
+    }
+}
